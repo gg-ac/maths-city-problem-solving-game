c23649b3e132c2586e09a459b38fca080bda26a1
gandrewgabriel <g.andrew.gabriel@gmail.com>
2024-10-23 09:16:36 +0100

    Removed unneeded imports

--- src/scenes/TestScene.ts
+++ src/scenes/TestScene.ts
@@ -4,11 +4,8 @@ import { SymbolFactory } from '../task/SymbolFactory';
 import { Symbol, TransformationRule } from '../task/StringTransformation';
 import { TaskTrialStringTransformation } from '../task/TaskTrialStringTransformation';
 import { StringState } from '../task/StringPanel';
-import { GazeAperture } from '../task/GazeAperture';
 import { DataStore } from '../task/DataStorage';
 import { StudySchedule } from '../constants/studySchedule';
-import { getFirestore } from 'firebase/firestore';
-import { getAuth } from 'firebase/auth';
 import { authorisedUser } from '../main';
 
 
@@ -38,46 +35,6 @@ export class TestScene extends BaseScene {
         this.currentTrialIndex = 0
         this.totalTrials = this.studySchedule.trials.length
         this.currentTrial = this.generateNextTrial(this.studySchedule, this.currentTrialIndex, this.symbolFactory)
-
-
-        // // this.title = this.add.text(20, 40, 'Hello', {
-        // //     fontFamily: 'Corbel', fontSize: 46, color: '#ffffff',
-        // //     stroke: '#000000', strokeThickness: 8,
-        // //     align: 'center'
-        // // }).setOrigin(0).setInteractive();
-
-        // // Register some symbol images with the symbol factory
-
-
-        // // Some test rules
-        // const r1_in = symbolFactory.getSymbolsByIDs(["2", "1", "3"])
-        // const r1_out = symbolFactory.getSymbolsByIDs(["1", "3"])
-        // const r1 = new TransformationRule(r1_in, r1_out)
-        // const r2_in = symbolFactory.getSymbolsByIDs(["5", "6"])
-        // const r2_out = symbolFactory.getSymbolsByIDs(["6", "5"])
-        // const r2 = new TransformationRule(r2_in, r2_out)
-        // const rules = [r2, r1]
-
-        // // An initial string
-        // const string_1 = ["2", "2", "1", "3", "4", "2"]
-        // const start_string = symbolFactory.getSymbolsByIDs(string_1)
-
-        // // A target string
-        // const targetString = symbolFactory.getSymbolsByIDs(["1", "3", "4"])
-
-        // // Some forbidden strings
-        // const forbiddenString1 = symbolFactory.getSymbolsByIDs(["3", "1", "4"])
-        // // const forbiddenString2 = symbolFactory.getSymbolsByIDs(["4", "2", "3", "1"])
-        // // const forbiddenString3 = symbolFactory.getSymbolsByIDs(["2", "1", "1", "4"])
-
-
-
-        // // The task instance handling state, graphics, and interaction
-        // const task = new TaskTrialStringTransformation(this, rules, new StringState(start_string, null), targetString, [forbiddenString1], symbolFactory, this.dataStore, () => this.onTrialComplete())
-
-        // const gazeAperture = new GazeAperture(this, 100)
-        // gazeAperture.setActive(false)
-
 
         super.create()
     }
