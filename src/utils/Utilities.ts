34b0dbf4e449404dade0eac7e902ec603af1e26f
gg-ac <gg-ac@users.noreply.github.com>
2025-02-07 13:34:28 +0000

    deploy: 14e836d372c7761d478692d7a0150308ff920880

--- src/utils/Utilities.ts
+++
@@ -1,6 +0,0 @@
-export function mapArrayToIndexedObject<T>(array: T[]): { [key: number]: T } {
-    return array.reduce((acc, curr, index) => {
-        acc[index] = curr;
-        return acc;
-    }, {} as { [key: number]: T });
-}
